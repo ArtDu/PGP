2cb6d1b8b9f1e93a8f7c77edfec09f37923af369
artyom dubinin <artyom.dubinin@corp.mail.ru>
2020-12-30 17:13:22 +0700

    new version

--- labs/lab8/main.cu
+++ labs/lab8/main.cu
@@ -14,7 +14,6 @@ do {                                \
   }                               \
 } while(0)
 
-int id, ib, jb, kb, nbx, nby, nbz, nx, ny, nz;
 
 // Индексация внутри блока
 // из 3d табличного представления в 1d одиночный массив
@@ -40,16 +39,16 @@ __global__ void kernel_copy_yz(double *plane_yz, double *data, int nx, int ny, i
     if (dir) {
         for (k = idy; k < nz; k += offsety)
             for (j = idx; j < ny; j += offsetx)
-                plane_yz[_iyz(j, k)] = data[_ib(i, j, k)];
+                plane_yz[_iyz(j, k)] = data[_i(i, j, k)];
     } else {
         if (plane_yz) {
             for (k = idy; k < nz; k += offsety)
                 for (j = idx; j < ny; j += offsetx)
-                    data[_ib(i, j, k)] = plane_yz[_iyz(j, k)];
+                    data[_i(i, j, k)] = plane_yz[_iyz(j, k)];
         } else {
             for (k = idy; k < nz; k += offsety)
                 for (j = idx; j < ny; j += offsetx)
-                    data[_ib(i, j, k)] = bc;
+                    data[_i(i, j, k)] = bc;
         }
     }
 }
@@ -63,16 +62,16 @@ __global__ void kernel_copy_xz(double *plane_xz, double *data, int nx, int ny, i
     if (dir) {
         for (k = idy; k < nz; k += offsety)
             for (i = idx; i < nx; i += offsetx)
-                plane_xz[_ixz(i, k)] = data[_ib(i, j, k)];
+                plane_xz[_ixz(i, k)] = data[_i(i, j, k)];
     } else {
         if (plane_xz) {
             for (k = idy; k < nz; k += offsety)
                 for (i = idx; i < nx; i += offsetx)
-                    data[_ib(i, j, k)] = plane_xz[_ixz(i, k)];
+                    data[_i(i, j, k)] = plane_xz[_ixz(i, k)];
         } else {
             for (k = idy; k < nz; k += offsety)
                 for (i = idx; i < nx; i += offsetx)
-                    data[_ib(i, j, k)] = bc;
+                    data[_i(i, j, k)] = bc;
         }
     }
 }
@@ -86,18 +85,52 @@ __global__ void kernel_copy_xy(double *plane_xy, double *data, int nx, int ny, i
     if (dir) {
         for (j = idy; j < ny; j += offsety)
             for (i = idx; i < nx; i += offsetx)
-                plane_xy[_ixy(i, j)] = data[_ib(i, j, k)];
+                plane_xy[_ixy(i, j)] = data[_i(i, j, k)];
     } else {
         if (plane_xy) {
             for (j = idy; j < ny; j += offsety)
                 for (i = idx; i < nx; i += offsetx)
-                    data[_ib(i, j, k)] = plane_xy[_ixy(i, j)];
+                    data[_i(i, j, k)] = plane_xy[_ixy(i, j)];
         } else {
             for (j = idy; j < ny; j += offsety)
                 for (i = idx; i < nx; i += offsetx)
-                    data[_ib(i, j, k)] = bc;
+                    data[_i(i, j, k)] = bc;
         }
     }
+}
+
+__global__ void kernel(double *next, double *data, int nx, int ny, int nz, double hx, double hy, double hz) {
+    int idz = blockIdx.z + blockDim.z + threadIdx.z;
+    int idy = blockIdx.y + blockDim.y + threadIdx.y;
+    int idx = blockIdx.x + blockDim.x + threadIdx.x;
+    int offsetz = blockDim.z + gridDim.z;
+    int offsety = blockDim.y + gridDim.y;
+    int offsetx = blockDim.x + gridDim.x;
+    int i, j, k;
+    for (i = idx; i < nx; i += offsetx)
+        for (j = idy; j < ny; j += offsety)
+            for (k = idz; k < nz; k += offsetz) {
+                next[_i(i, j, k)] = 0.5 * ((data[_i(i + 1, j, k)] + data[_i(i - 1, j, k)]) / (hx * hx) +
+                                           (data[_i(i, j + 1, k)] + data[_i(i, j - 1, k)]) / (hy * hy) +
+                                           (data[_i(i, j, k + 1)] + data[_i(i, j, k - 1)]) / (hz * hz)) /
+                                    (1.0 / (hx * hx) + 1.0 / (hy * hy) + 1.0 / (hz * hz));
+            }
+}
+
+__global__ void kernel_error(double *next, double *data, int nx, int ny, int nz) {
+    int idz = blockIdx.z + blockDim.z + threadIdx.z;
+    int idy = blockIdx.y + blockDim.y + threadIdx.y;
+    int idx = blockIdx.x + blockDim.x + threadIdx.x;
+    int offsetz = blockDim.z + gridDim.z;
+    int offsety = blockDim.y + gridDim.y;
+    int offsetx = blockDim.x + gridDim.x;
+    int i, j, k;
+    for (i = idx - 1; i < nx + 1; i += offsetx)
+        for (j = idy - 1; j < ny + 1; j += offsety)
+            for (k = idz - 1; k < nz + 1; k += offsetz) {
+                next[_i(i, j, k)] = ((i != -1 && j != -1 && k != -1 && i != nx && j != ny && k != nz))
+                                    * fabs(next[_i(i, j, k)] - data[_i(i, j, k)]);
+            }
 }
 
 int main(int argc, char *argv[]) {
@@ -105,12 +138,14 @@ int main(int argc, char *argv[]) {
 //    std::cin.tie(nullptr);
 //    std::cout.tie(nullptr);
 
-
+    int id, ib, jb, kb, nbx, nby, nbz, nx, ny, nz;
     int i, j, k;
     int numproc, proc_name_len;
     char proc_name[MPI_MAX_PROCESSOR_NAME];
     double lx, ly, lz, hx, hy, hz, bc_down, bc_up, bc_left, bc_right, bc_front, bc_back, eps, diff, u_0;
-    double *data, *temp, *next, *buff;
+    double *data, *temp, *next, *buff, *plane_xy, *plane_xz, *plane_yz;
+    double *dev_data, *dev_next, *dev_plane_xy, *dev_plane_xz, *dev_plane_yz;
+
     std::string file_name;
 
     MPI_Status status;
@@ -156,16 +191,30 @@ int main(int argc, char *argv[]) {
     hy = ly / (ny * nby);
     hz = lz / (nz * nbz);
 
-    data = (double *) malloc(sizeof(double) * (nx + 2) * (ny + 2) * (nz + 2));
-    next = (double *) malloc(sizeof(double) * (nx + 2) * (ny + 2) * (nz + 2));
-    buff = (double *) malloc(sizeof(double) *
-                             (std::max(nx, std::max(ny, nz)) * std::max(nx, std::max(ny, nz)) + 2));
+    int _size_b = (nx + 2) * (ny + 2) * (nz + 2);
+    int _size_plane = (std::max(nx, std::max(ny, nz)) * std::max(nx, std::max(ny, nz)) + 2);
+    data = (double *) malloc(sizeof(double) * _size_b);
+    next = (double *) malloc(sizeof(double) * _size_b);
+    buff = (double *) malloc(sizeof(double) * _size_plane);
+    plane_xz = (double *) malloc(sizeof(double) * _size_plane);
+    plane_xy = (double *) malloc(sizeof(double) * _size_plane);
+    plane_yz = (double *) malloc(sizeof(double) * _size_plane);
 
+    CSC(cudaMalloc(&dev_data, sizeof(double) * _size_b));
+    CSC(cudaMalloc(&dev_next, sizeof(double) * _size_b));
+    CSC(cudaMalloc(&dev_plane_xz, sizeof(double) * _size_plane));
+    CSC(cudaMalloc(&dev_plane_xy, sizeof(double) * _size_plane));
+    CSC(cudaMalloc(&dev_plane_yz, sizeof(double) * _size_plane));
 
     for (i = 0; i < nx; i++)          // Инициализация блока
         for (j = 0; j < ny; j++)
             for (k = 0; k < nz; k++)
                 data[_i(i, j, k)] = u_0;
+
+    CSC(cudaMemcpy(dev_data, data, sizeof(double) * _size_b, cudaMemcpyHostToDevice));
+
+    dim3 blocks(32, 32);
+    dim3 threads(32, 32);
 
     for (; true;) {
         diff = 0;
@@ -173,6 +222,9 @@ int main(int argc, char *argv[]) {
 // Отправка данных и прием в перемешку
 
         if (ib + 1 < nbx) {
+//            kernel_copy_yz<<<blocks, threads>>> (dev_plane_yz, dev_data, nx, ny, nz, nx - 1, true, 0.0);
+//            CSC(cudaGetLastError());
+//            CSC(cudaMemcpy(buff, dev_plane_yz, sizeof(double) * _size_plane, cudaMemcpyDeviceToHost));
             for (k = 0; k < nz; ++k)
                 for (j = 0; j < ny; j++)
                     buff[k * ny + j] = data[_i(nx - 1, j, k)];
@@ -192,6 +244,9 @@ int main(int argc, char *argv[]) {
         }
 
         if (jb + 1 < nby) {
+//            kernel_copy_xz<<<blocks, threads>>> (dev_plane_xz, dev_data, nx, ny, nz, ny - 1, true, 0.0);
+//            CSC(cudaGetLastError());
+//            CSC(cudaMemcpy(buff, dev_plane_xz, sizeof(double) * _size_plane, cudaMemcpyDeviceToHost));
             for (k = 0; k < nz; ++k)
                 for (i = 0; i < nx; i++)
                     buff[k * nx + i] = data[_i(i, ny - 1, k)];
@@ -211,6 +266,10 @@ int main(int argc, char *argv[]) {
         }
 
         if (kb + 1 < nbz) {
+//            CSC(cudaMemcpy(dev_data, data, sizeof(double) * _size_b, cudaMemcpyHostToDevice));
+//            kernel_copy_xy<<<blocks, threads>>> (dev_plane_xy, dev_data, nx, ny, nz, nz - 1, true, 0.0);
+//            CSC(cudaGetLastError());
+//            CSC(cudaMemcpy(buff, dev_plane_xy, sizeof(double) * _size_plane, cudaMemcpyDeviceToHost));
             for (j = 0; j < ny; ++j)
                 for (i = 0; i < nx; i++)
                     buff[j * nx + i] = data[_i(i, j, nz - 1)];
@@ -230,6 +289,9 @@ int main(int argc, char *argv[]) {
         }
 
         if (ib > 0) {
+//            kernel_copy_yz<<<blocks, threads>>> (dev_plane_yz, dev_data, nx, ny, nz, 0, true, 0.0);
+//            CSC(cudaGetLastError());
+//            CSC(cudaMemcpy(buff, dev_plane_yz, sizeof(double) * _size_plane, cudaMemcpyDeviceToHost));
             for (k = 0; k < nz; ++k)
                 for (j = 0; j < ny; j++)
                     buff[k * ny + j] = data[_i(0, j, k)];
@@ -250,6 +312,9 @@ int main(int argc, char *argv[]) {
 
 
         if (jb > 0) {
+//            kernel_copy_xz<<<blocks, threads>>> (dev_plane_xz, dev_data, nx, ny, nz, 0, true, 0.0);
+//            CSC(cudaGetLastError());
+//            CSC(cudaMemcpy(buff, dev_plane_xz, sizeof(double) * _size_plane, cudaMemcpyDeviceToHost));
             for (k = 0; k < nz; ++k)
                 for (i = 0; i < nx; i++)
                     buff[k * nx + i] = data[_i(i, 0, k)];
